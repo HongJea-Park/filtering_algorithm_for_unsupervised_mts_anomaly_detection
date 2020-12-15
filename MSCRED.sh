b4d5752dba393dedc33892f6b252934b5f5b979e
HongJea-Park <hongjea.park@ds.seoultech.ac.kr>
2020-12-16 04:09:26 +0900

    modified shell script for MSCRED

--- MSCRED.sh
+++ MSCRED.sh
@@ -169,73 +169,73 @@ python main.py \
     --anomaly_ratio 0.005
 
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.005 \
     --iqr_multiplier 1.5
     
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.005 \
     --iqr_multiplier 3
     
 # ------------------------------ MSCRED ------------------------------
-# -------------------------  anomaly type 1 -------------------------
+# -------------------------  anomaly type 2 -------------------------
 # ----------------------------- ratio 0.01 ------------------------------
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering False \
     --anomaly_ratio 0.01
 
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.01 \
     --iqr_multiplier 1.5
     
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.01 \
     --iqr_multiplier 3
     
 # ------------------------------ MSCRED ------------------------------
-# -------------------------  anomaly type 1 -------------------------
+# -------------------------  anomaly type 2 -------------------------
 # ----------------------------- ratio 0.05 ------------------------------
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering False \
     --anomaly_ratio 0.05
 
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.05 \
     --iqr_multiplier 1.5
     
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.05 \
     --iqr_multiplier 3
     
 # ------------------------------ MSCRED ------------------------------
-# -------------------------  anomaly type 1 -------------------------
+# -------------------------  anomaly type 2 -------------------------
 # ----------------------------- ratio 0.1 ------------------------------
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering False \
     --anomaly_ratio 0.1
 
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.1 \
     --iqr_multiplier 1.5
     
 python main.py \
-    --anomaly_type type1 \
+    --anomaly_type type2 \
     --filtering True \
     --anomaly_ratio 0.1 \
     --iqr_multiplier 3
